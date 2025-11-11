f115e88c11dcab325e9d90dc2244fd00f2192888
wiseman-s <allinmer57@gmail.com>
2025-11-11 14:45:28 +0300

    app.py

--- app.py
+++ app.py
@@ -395,13 +395,11 @@ elif page == "Drug Discovery Lab 💊":
     color = "#4CC3D9" if p["Efficacy"] < 60 else "#7CFC00" if p["Efficacy"] < 80 else "#228B22"
     xpos = i * 1.8 - 1.8
     a_html += f"""
-      <a-box position="{xpos} 0.25 -3" depth="0.5" height="0.5" width="0.5" color="{color}">
-        <a-animation attribute="rotation" dur="8000" to="0 360 0" repeat="indefinite"></a-animation>
-      </a-box>
-      <a-text value="{p['Compound']}" look-at="[camera]" align="center"
-              position="{xpos} 2.8 -3" color="#111" scale="1.3 1.3 1.3"></a-text>
-      <a-text value="Stroke Reduction: {p['StrokeReduction']}%" look-at="[camera]" align="center"
-              position="{xpos} 2.3 -3" color="#333" scale="1.1 1.1 1.1"></a-text>
+      <a-box position="{xpos} 0.25 -3" depth="0.5" height="0.5" width="0.5" color="{color}"></a-box>
+      <a-text value="{p['Compound']}" align="center" position="{xpos} 1.2 -3" color="#111"></a-text>
+      <a-text value="Stroke Reduction: {p['StrokeReduction']}%" align="center" position="{xpos} 0.7 -3" color="#333"></a-text>
+    """
+
     """
 
     """
